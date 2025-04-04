9e7fff66b2db36fa0d2e622573f56f682d0c8ace
QUENTIN.Rigagneau <rga.quentin@gmail.com>
2025-04-04 14:26:42 +0200

    Kinematic set world matrix added in python

--- rb_kinematic_set_world_matrix.py
+++ rb_kinematic_set_world_matrix.py
@@ -2,7 +2,6 @@ import harfang as hg
 import math
 
 
-# Function to create a physics-enabled cube
 def create_physic_cube_ex(scene, size, mtx, model_ref, materials, rb_type, mass):
     node = hg.CreateObject(scene, mtx, model_ref, materials)
     node.SetName("Physic Cube")
@@ -10,64 +9,73 @@ def create_physic_cube_ex(scene, size, mtx, model_ref, materials, rb_type, mass)
     rb.SetType(rb_type)
     node.SetRigidBody(rb)
 
+    # Création d'une collision en forme de cube
     col = scene.CreateCollision()
     col.SetType(hg.CT_Cube)
     col.SetSize(size)
     col.SetMass(mass)
 
+    # Assigner la collision au cube
     node.SetCollision(0, col)
     return node, rb
 
 
-hg.AddAssetsFolder('assets_compiled')
+hg.AddAssetsFolder("assets_compiled")
 
-# Main window setup
+# Initialisation des entrées et de la fenêtre
 hg.InputInit()
 hg.WindowSystemInit()
 
 res_x, res_y = 1280, 720
-win = hg.RenderInit('Physics Test', res_x, res_y, hg.RF_VSync | hg.RF_MSAA4X)
+win = hg.RenderInit("Physics Test", res_x, res_y, hg.RF_VSync | hg.RF_MSAA4X)
 
 pipeline = hg.CreateForwardPipeline()
 res = hg.PipelineResources()
 
-# Physics debug
+# Debug physics
 vtx_line_layout = hg.VertexLayoutPosFloatColorUInt8()
 line_shader = hg.LoadProgramFromAssets("shaders/pos_rgb")
 
-# Create material
-pbr_shader = hg.LoadPipelineProgramRefFromAssets('core/shader/pbr.hps', res, hg.GetForwardPipelineInfo())
-mat_grey = hg.CreateMaterial(pbr_shader, 'uBaseOpacityColor', hg.Vec4(1, 1, 1), 'uOcclusionRoughnessMetalnessColor',
-                             hg.Vec4(1, 0.5, 0.05))
+# Création du matériau
+pbr_shader = hg.LoadPipelineProgramRefFromAssets("core/shader/pbr.hps", res, hg.GetForwardPipelineInfo())
+mat_grey = hg.CreateMaterial(pbr_shader, "uBaseOpacityColor", hg.Vec4(1, 1, 1),
+                             "uOcclusionRoughnessMetalnessColor", hg.Vec4(1, 0.5, 0.05))
 
-# Create models
+# Création des modèles
 vtx_layout = hg.VertexLayoutPosFloatNormUInt8()
 
+# Cube
 cube_size = hg.Vec3(1, 1, 1)
-cube_ref = res.AddModel('cube', hg.CreateCubeModel(vtx_layout, cube_size.x, cube_size.y, cube_size.z))
+cube_ref = res.AddModel("cube", hg.CreateCubeModel(vtx_layout, cube_size.x, cube_size.y, cube_size.z))
 
+# Sol
 ground_size = hg.Vec3(4, 0.05, 4)
-ground_ref = res.AddModel('ground', hg.CreateCubeModel(vtx_layout, ground_size.x, ground_size.y, ground_size.z))
+ground_ref = res.AddModel("ground", hg.CreateCubeModel(vtx_layout, ground_size.x, ground_size.y, ground_size.z))
 
-# Setup the scene
+# Configuration de la scène
 scene = hg.Scene()
 
+# Création de la caméra
 cam_mat = hg.TransformationMat4(hg.Vec3(0, 1.5, -5), hg.Vec3(hg.Deg(20), 0, 0))
 cam = hg.CreateCamera(scene, cam_mat, 0.01, 1000)
 view_matrix = hg.InverseFast(cam_mat)
 c = cam.GetCamera()
-projection_matrix = hg.ComputePerspectiveProjectionMatrix(c.GetZNear(), c.GetZFar(), hg.FovToZoomFactor(c.GetFov()),
+projection_matrix = hg.ComputePerspectiveProjectionMatrix(c.GetZNear(), c.GetZFar(),
+                                                          hg.FovToZoomFactor(c.GetFov()),
                                                           hg.Vec2(res_x / res_y, 1))
 
 scene.SetCurrentCamera(cam)
 
+# Lumière
 lgt = hg.CreateLinearLight(scene, hg.TransformationMat4(hg.Vec3(0, 0, 0), hg.Vec3(hg.Deg(30), hg.Deg(30), 0)),
-                           hg.Color(1, 1, 1), hg.Color(1, 1, 1), 10, hg.LST_Map, 0.0001, hg.Vec4(2, 4, 10, 16))
+                           hg.Color(1, 1, 1), hg.Color(1, 1, 1), 10, hg.LST_Map, 0.0001,
+                           hg.Vec4(2, 4, 10, 16))
 
-cube_node, _ = create_physic_cube_ex(scene, cube_size, hg.TranslationMat4(hg.Vec3(0, 0, 0)), cube_ref, [mat_grey],
-                                     hg.RBT_Kinematic, 1.0)
+# Création d'un cube physique
+cube_node, _ = create_physic_cube_ex(scene, cube_size, hg.TranslationMat4(hg.Vec3(0, 0, 0)),
+                                     cube_ref, [mat_grey], hg.RBT_Kinematic, 1.0)
 
-# Scene physics
+# Initialisation de la physique
 physics = hg.SceneBullet3Physics()
 physics.SceneCreatePhysicsFromAssets(scene)
 physics_step = hg.time_from_sec_f(1 / 60)
@@ -76,22 +84,25 @@ dt_frame_step = hg.time_from_sec_f(1 / 60)
 clocks = hg.SceneClocks()
 
 hg.SetLogLevel(hg.LL_Normal)
-print(
-    ">>> Description:\n>>> Set the position and rotation of a kinematic cube in the rendering loop. The cube shall move from left to right and rotate on its Y axis.")
+print(">>> Description:\n>>> Déplace un cube cinématique de gauche à droite et le fait tourner sur son axe Y.")
 
-# Main loop
+# Boucle principale
 keyboard = hg.Keyboard()
 frame_count = 0
 
 while not keyboard.Down(hg.K_Escape) and hg.IsWindowOpen(win):
     keyboard.Update()
 
-    cube_node.GetTransform().SetPos(hg.Vec3((frame_count % 200 - 100) / 100.0, 0.0, 0.0))
-    cube_node.GetTransform().SetRot(hg.Vec3(0.0, math.pi * frame_count / 360.0, 0.0))
+    # Position et rotation du cube
+    _pos = hg.Vec3((math.fmod(frame_count, 200.0) - 100.0) / 100.0, 0.0, 0.0)
+    _rot = hg.Vec3(0.0, math.pi * frame_count / 360.0, 0.0)
+    cube_node.GetTransform().SetWorld(hg.TransformationMat4(_pos, _rot))
 
+    # Mise à jour de la scène
     view_id = 0
     hg.SceneUpdateSystems(scene, clocks, dt_frame_step, physics, physics_step, 3)
-    view_id, pass_id = hg.SubmitSceneToPipeline(view_id, scene, hg.IntRect(0, 0, res_x, res_y), True, pipeline, res)
+    view_id, pass_id = hg.SubmitSceneToPipeline(view_id, scene, hg.IntRect(0, 0, res_x, res_y),
+                                                True, pipeline, res)
 
     # Debug physics display
     hg.SetViewClear(view_id, 0, 0, 1.0, 0)
@@ -105,13 +116,11 @@ while not keyboard.Down(hg.K_Escape) and hg.IsWindowOpen(win):
     hg.Frame()
     hg.UpdateWindow(win)
 
+# Nettoyage
 scene.Clear()
 scene.GarbageCollect()
-
 hg.RenderShutdown()
 hg.DestroyWindow(win)
 
 hg.WindowSystemShutdown()
 hg.InputShutdown()
-
-
